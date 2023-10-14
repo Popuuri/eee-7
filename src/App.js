17f5052ce4db6579d741851db137e483a3bf989e
popuuri <popurimourya@gmail.com>
2023-10-14 19:32:38 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
-  return (
+ return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>My Portfolio</h1>
       </header>
+      <main>
+        <section className="section">
+          <h2>About Me</h2>
+          <p>
+            Hi, I'm Mourya. I'm a Web Developer based in Chennai.
+          </p>
+        </section>
+        <section className="section">
+          <h2>My Skills</h2>
+          <ul>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>JavaScript</li>
+            <li>React</li>
+          </ul>
+        </section>
+        <section className="section">
+          <h2>My Projects</h2>
+          <div className="project">
+            <h3>Project 1</h3>
+            <p>Created a React APP</p>
+          </div>
+          <div className="project">
+            <h3>Project 2</h3>
+            <p>Craeted a Website</p>
+          </div>
+        </section>
+      </main>
     </div>
-  );
+ );
 }
 
 export default App;
